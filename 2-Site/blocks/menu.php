905c5c79a3543fc9f042f2d723a223aea7484258
usarb <chilatsergiu@gmail.com>
2019-11-13 16:57:06 +0200

    Crearea unui site static

---
+++ 2-Site/blocks/menu.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="index.php">HOME</a></li>
+    <li><a href="news.php">News</a></li>
+    <li><a href="photos.php">Photos</a></li>
+    <li><a href="contact.php">Contact</a></li>
+</ul>
---
+++ 2-Site/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/site.css">
+</head>
+<body>
+    <nav>
+        <? include 'blocks/menu.php';?>
+    </nav>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id tortor risus. Phasellus tellus magna, aliquet eu elit id, euismod sagittis metus. Ut semper bibendum sollicitudin. In erat augue, molestie in rutrum in, egestas sit amet nisl. Donec posuere dapibus mi sit amet semper. Cras non tellus nec sapien venenatis.
+</body>
+</html>
